ab4886a7f2895c1a6c7e4449d27a1fe329b90b38
David Yam <dyyam02@gmail.com>
2023-11-18 09:42:56 -0800

    Allow recording by holding on button

--- recordAudio.py
+++ recordAudio.py
@@ -1,17 +1,18 @@
 import pyaudio
 import wave
+import keyboard
 
 
 chunk = 1024  # Record in chunks of 1024 samples
 sample_format = pyaudio.paInt16  # 16 bits per sample
 channels = 1
 fs = 44100  # Record at 44100 samples per second
-seconds = 3
 filename = "sounds/output.wav"
+
 
 p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-print('Recording')
+
 
 stream = p.open(format=sample_format,
                 channels=channels,
@@ -21,10 +22,13 @@ stream = p.open(format=sample_format,
 
 frames = []  # Initialize array to store frames
 
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs / chunk * seconds)):
-    data = stream.read(chunk)
-    frames.append(data)
+print("Hold down [SPACE] to record audio!")
+keyboard.wait("space")
+if keyboard.is_pressed("space"): 
+    print('Recording...')
+    while keyboard.is_pressed("space"):
+        data = stream.read(chunk)                                        
+        frames.append(data)
 
 # Stop and close the stream 
 stream.stop_stream()
@@ -32,7 +36,7 @@ stream.close()
 # Terminate the PortAudio interface
 p.terminate()
 
-print('Finished recording')
+print('Finished recording!')
 print(len(frames))
 
 # Save the recorded data as a WAV file
