e265a9cf4efa22ceb0dcdb55231b7e4465845061
Vincent <spotandroxy3@gmail.com>
2023-11-18 19:00:25 -0800

    reorganized global variables

--- miccheck.py
+++ miccheck.py
@@ -6,26 +6,20 @@ import pygame
 import recordAudio as ra
 from time import sleep
 
-
 #Setting up the Game
 pygame.init
 screen = pygame.display.set_mode((1280,720), pygame.RESIZABLE)
 pygame.display.set_caption('Mic Checkers')
-bg = pygame.image.load('./images/miccheck_bg.png')
-icon = pygame.image.load('./images/miccheck_icon.png')
-pygame.display.set_icon(icon)
+pygame.display.set_icon(pygame.image.load('./images/miccheck_icon.png'))
 clock = pygame.time.Clock()
 pygame.font.init()
+
 
 buttonSizeScale = (50, 50)
 redplaybutton = pygame.transform.scale(pygame.image.load("images/redplay.png"), buttonSizeScale)
 greenplaybutton = pygame.transform.scale(pygame.image.load("images/greenplay.png"), buttonSizeScale)
 title_playbutton = pygame.image.load("./images/play_button.png")
-choice1 = pygame.transform.scale(pygame.image.load("images/choice1.png"), buttonSizeScale)
-choice2 = pygame.transform.scale(pygame.image.load("images/choice2.png"), buttonSizeScale)
-choice3 = pygame.transform.scale(pygame.image.load("images/choice3.png"), buttonSizeScale)
-choice4 = pygame.transform.scale(pygame.image.load("images/choice4.png"), buttonSizeScale)
-choice5 = pygame.transform.scale(pygame.image.load("images/choice5.png"), buttonSizeScale)
+
 checkmark = pygame.transform.scale(pygame.image.load("images/checkmark.png"), buttonSizeScale)
 
 speakingSizeScale = (150, 150)
@@ -42,7 +36,6 @@ SCREEN_WIDTH, SCREEN_HEIGHT = screen.get_size()
 leaveScreen = False
 running = True
 choice = "wrong"
-color = (245, 245, 220)
 window = 0
 
 def title_screen():
@@ -79,6 +72,53 @@ def title_screen():
         pygame.display.update()
         clock.tick(60)
     
+def howto_screen():
+    global running
+    global leaveScreen
+
+    #Setting the Text Size, Font, and Placement
+    title_font = pygame.font.SysFont('Comic Sans MS', 40)
+    title = title_font.render('How to Play', True, (0, 0, 0))
+    title_rect = title.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/16))
+
+    instruction_font = pygame.font.SysFont('Comic Sans MS', 20)
+    instruction1 = instruction_font.render('To begin the game, one person is given a prompt and', True, (0, 0, 0))
+    instruction_rect1 = instruction1.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7))
+    instruction2 = instruction_font.render('must record an clip of their best impression. Their', True, (0, 0, 0))
+    instruction_rect2 = instruction2.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 25))
+    instruction3 = instruction_font.render('clip is shuffled alongside multiple clips of the', True, (0, 0, 0))
+    instruction_rect3 = instruction3.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 50))
+    instruction4 = instruction_font.render('actual prompt where the rest have to identify', True, (0, 0, 0))
+    instruction_rect4 = instruction4.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 75))
+    instruction5 = instruction_font.render('which one is the imposter!', True, (0, 0, 0))
+    instruction_rect5 = instruction5.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 100))
+
+
+    icon = pygame.transform.scale(pygame.image.load("images/miccheck_bg.png"), (350, 300))
+
+    leaveScreen = False
+
+    while running and not leaveScreen:
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
+                running = False
+    
+        #Rendering new things onto screen
+        screen.fill((100,100,126))
+        screen.blit(title, title_rect)
+        screen.blit(icon, icon.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/3)))
+        screen.blit(instruction1, instruction_rect1)
+        screen.blit(instruction2, instruction_rect2)
+        screen.blit(instruction3, instruction_rect3)
+        screen.blit(instruction4, instruction_rect4)
+        screen.blit(instruction5, instruction_rect5)
+        submitButton(SCREEN_WIDTH/1.5, SCREEN_HEIGHT/1.2, submitSizeScale[0], submitSizeScale[1], titlebutton, 0)
+
+        #Updating displaying the new screen
+        pygame.display.update()
+        clock.tick(60)
+
 
 def recording_screen():
     title_font = pygame.font.SysFont('Comic Sans MS', 40)
@@ -115,7 +155,7 @@ def recording_screen():
 
 
         #Rendering new things onto screen (blit puts texts and images onto screen)
-        screen.fill(color)
+        screen.fill((245, 245, 220))
         screen.blit(title, title_rect)
         screen.blit(prompt, prompt_rect)
         screen.blit(sample, sample_rect)
@@ -134,6 +174,12 @@ def guessing_screen():
     global running
     global leaveScreen
     global selected
+
+    choice1 = pygame.transform.scale(pygame.image.load("images/choice1.png"), buttonSizeScale)
+    choice2 = pygame.transform.scale(pygame.image.load("images/choice2.png"), buttonSizeScale)
+    choice3 = pygame.transform.scale(pygame.image.load("images/choice3.png"), buttonSizeScale)
+    choice4 = pygame.transform.scale(pygame.image.load("images/choice4.png"), buttonSizeScale)
+    choice5 = pygame.transform.scale(pygame.image.load("images/choice5.png"), buttonSizeScale)
 
 
     #Setting the Text Size, Font, and Placement
@@ -179,55 +225,6 @@ def guessing_screen():
         clock.tick(60)
 
 
-def howto_screen():
-    global running
-    global leaveScreen
-
-    #Setting the Text Size, Font, and Placement
-    title_font = pygame.font.SysFont('Comic Sans MS', 40)
-    title = title_font.render('How to Play', True, (0, 0, 0))
-    title_rect = title.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/16))
-
-    instruction_font = pygame.font.SysFont('Comic Sans MS', 20)
-    instruction1 = instruction_font.render('To begin the game, one person is given a prompt and', True, (0, 0, 0))
-    instruction_rect1 = instruction1.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7))
-    instruction2 = instruction_font.render('must record an clip of their best impression. Their', True, (0, 0, 0))
-    instruction_rect2 = instruction2.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 25))
-    instruction3 = instruction_font.render('clip is shuffled alongside multiple clips of the', True, (0, 0, 0))
-    instruction_rect3 = instruction3.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 50))
-    instruction4 = instruction_font.render('actual prompt where the rest have to identify', True, (0, 0, 0))
-    instruction_rect4 = instruction4.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 75))
-    instruction5 = instruction_font.render('which one is the imposter!', True, (0, 0, 0))
-    instruction_rect5 = instruction5.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/1.7 + 100))
-
-
-    icon = pygame.transform.scale(pygame.image.load("images/miccheck_bg.png"), (350, 300))
-
-    leaveScreen = False
-
-    while running and not leaveScreen:
-        keys = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
-                running = False
-    
-        #Rendering new things onto screen
-        screen.fill((100,100,126))
-        screen.blit(title, title_rect)
-        screen.blit(icon, icon.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/3)))
-        screen.blit(instruction1, instruction_rect1)
-        screen.blit(instruction2, instruction_rect2)
-        screen.blit(instruction3, instruction_rect3)
-        screen.blit(instruction4, instruction_rect4)
-        screen.blit(instruction5, instruction_rect5)
-        
-        
-        submitButton(SCREEN_WIDTH/1.5, SCREEN_HEIGHT/1.2, submitSizeScale[0], submitSizeScale[1], titlebutton, 0)
-
-        #Updating displaying the new screen
-        pygame.display.update()
-        clock.tick(60)
-
 
 def end_screen():
     global running
@@ -263,8 +260,8 @@ def end_screen():
             screen.blit(subtitle, subtitle_rect)
 
         playSoundButton(SCREEN_WIDTH/2.1, SCREEN_HEIGHT/4, buttonSizeScale[0], buttonSizeScale[1], play, "sounds/output.wav")
-        submitButton(SCREEN_WIDTH/2.6, SCREEN_HEIGHT/2, submitSizeScale[0], submitSizeScale[1], title_button, 0)
-        submitButton(SCREEN_WIDTH/2.6, SCREEN_HEIGHT/1.5, submitSizeScale[0], submitSizeScale[1], quit_button, -1)
+        submitButton(SCREEN_WIDTH/2.6, SCREEN_HEIGHT/2.2, submitSizeScale[0], submitSizeScale[1], title_button, 0)
+        submitButton(SCREEN_WIDTH/2.6, SCREEN_HEIGHT/1.6, submitSizeScale[0], submitSizeScale[1], quit_button, -1)
 
         #Updating displaying the new screen
         pygame.display.update()
